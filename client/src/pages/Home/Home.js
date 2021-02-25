753a3d5c8202dfc94a31a8011e5a2fbd5c9cf210
Tucker2015 <mail@kevtucker.com>
2021-02-25 01:34:10 +0000

    - Test push from development

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -34,9 +34,6 @@ const Home = ({ auth, isLoading }) => {
         <div className="textContainer">
           <h2>Welcome to MixShare Live</h2>
           <h4>Live Video streaming platform for DJ's and Creators.</h4>
-          <br></br>
-          <p>We are just getting the final touches done to the site but you can still watch our content and live stream while we are ready.</p>
-          <br></br>
         </div>
         <hr></hr>
       </div>
